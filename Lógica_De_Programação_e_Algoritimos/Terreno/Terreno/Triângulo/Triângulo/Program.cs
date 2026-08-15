20b1f6ce2614a05b106b7002b6161331d5990df5
Thalyson <thalysonsouzasantos5@gmail.com>
2026-08-15 16:37:05 -0300

    Add files via upload

---
+++ Lógica_De_Programação_e_Algoritimos/Terreno/Terreno/Triângulo/Triângulo/Program.cs
@@ -0,0 +1,24 @@
+﻿try
+{
+    //Entrada de dados
+    Console.Write("Digite o valor de A: ");
+    double a = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Digite o valor de B: ");
+    double b = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Digite o valor de C:");
+    double c = Convert.ToDouble(Console.ReadLine());
+    // processamento de dados
+    if (a < b + c && b < a + c && c < b + c)
+    {
+        double perimetro = a + b + c;
+        Console.WriteLine($"perimetro = {perimetro:F1}");
+    }
+    else
+    {
+        double area = (a + b + c) / 2;
+    }
+}
+catch
+{
+    Console.WriteLine("Degite somente os numeros!");
+}
