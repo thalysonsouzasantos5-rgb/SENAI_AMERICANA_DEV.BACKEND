02415c2c97ba433fe70a1d8e7e587ab0cd4c8900
Thalyson <thalysonsouzasantos5@gmail.com>
2026-08-25 12:33:39 -0300

    Add files via upload

---
+++ Lógica_De_Programação_e_Algoritimos/Loop While/Loop While/Program.cs
@@ -0,0 +1,26 @@
+﻿//Declaração de variaveis
+int turistaNoParque = 0;
+int veiculoNoParque = 0;
+
+string movimento = Console.ReadLine();
+
+//Processamento de dados
+while (movimento != "Sair")
+{
+	int turista = Convert.ToInt32(movimento);
+    if (movimento == "entrando")
+	{
+		turistaNoParque += turistaNoParque;
+        veiculoNoParque++;
+		
+	}
+	else if (movimento == "voltando")
+	{
+        turistaNoParque -= turistaNoParque;
+        veiculoNoParque--;
+		
+	}
+	movimento = Console.ReadLine();
+}
+Console.WriteLine($"Turista: {turistaNoParque}");
+Console.WriteLine($"Veiculos: {veiculoNoParque}");
